56df3d666de7197b05cc7c1bed36626f1f58f4e5
Tim Miller <tim.miller@elsevier.com>
2025-06-24 12:23:57 -0400

    wip

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  #backend "s3" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.100"
+    }
+  }
+
+  required_version = "~> 1.8"
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  #allowed_account_ids = [var.aws_config.account_id]
+
+  default_tags {
+    tags = { "CreatedBy" = "Innovation CodeBuild"}
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+#variable "aws_account_id" {
+#  description = "AWS Account ID"
+#}
+#
+variable "aws_region" {
+  description = "AWS Region"
+  default = "us-east-2"
+}
