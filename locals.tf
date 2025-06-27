837b0a158430e6620ada81e4f04d07358b2d9c13
Tim Miller <tim.miller@elsevier.com>
2025-06-27 12:03:11 -0400

    updates

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  resource_name_prefix = "innovation-pattern-${var.environment_name}"
+  resource_name_prefix = "innov-${var.environment_name}"
 
   tags = {
     "Description" = "Terraform module for deploying an Apache web server."
