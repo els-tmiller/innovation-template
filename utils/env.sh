b60d169b854ff0ba6b375a1681071196f9279c69
Tim Miller <tim.miller@elsevier.com>
2025-07-03 16:03:01 -0400

    update env.sh

--- utils/env.sh
+++ utils/env.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# This file is used set variables needed for local pattern development
+
 export S3_BACKEND_BUCKET=
 export S3_BACKEND_KEY=terraform.tfstate
 export S3_BACKEND_REGION=
