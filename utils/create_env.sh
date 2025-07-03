21bdd1482c4c9e67784e9767dd168262c3b90b62
Tim Miller <tim.miller@elsevier.com>
2025-07-03 16:15:12 -0400

    add env helper

---
+++ utils/create_env.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Define the output script filename
+OUTPUT_FILE="pattern_env.sh"
+
+# Check if the output script already exists
+if [ -f "$OUTPUT_FILE" ]; then
+  echo "The file '$OUTPUT_FILE' already exists. Exiting."
+  exit 1
+fi
+
+# Prompt for environment variables
+read -p "Enter value for S3_BACKEND_BUCKET: " S3_BACKEND_BUCKET
+read -p "Enter value for S3_BACKEND_KEY [default: terraform.tfstate]:" S3_BACKEND_KEY
+read -p "Enter value for AWS_SECRET_ACCESS_KEY: " AWS_SECRET_ACCESS_KEY
+read -p "Enter value for ENVIRONMENT_NAME: " ENVIRONMENT_NAME
+read -p "Enter value for TARGET_REGION: " TARGET_REGION
+
+# Set default for S3_BACKEND_KEY if empty
+S3_BACKEND_KEY=${S3_BACKEND_KEY:-terraform.tfstate}
+
+# Create the output script with export commands
+cat <<EOF > "$OUTPUT_FILE"
+#!/bin/bash
+export AWS_REGION="$AWS_REGION"
+export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+export S3_BUCKET="$S3_BUCKET"
+export S3_BACKEND_KEY="$S3_BACKEND_KEY"
+EOF
+
+# Make the script executable
+chmod +x "$OUTPUT_FILE"
+
+echo "Environment variables script '$OUTPUT_FILE' has been created."
+
--- utils/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This file is used set variables needed for local pattern development
-
-export S3_BACKEND_BUCKET=
-export S3_BACKEND_KEY=terraform.tfstate
-export S3_BACKEND_REGION=
-export ENVIRONMENT_NAME=
-export TARGET_REGION=
